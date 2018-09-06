6582184c9c7fe10980aebc19ee89d0735ff50461
Alex Gold <gold_alex@bah.com>
2018-09-06 17:14:23 -0400

    Better generic use

--- R/pdf_page_funs.R
+++ R/pdf_page_funs.R
@@ -4,17 +4,8 @@
 # Pdf Page Constructor, Getters, Setters   #
 ############################################
 
-#' Create a PDF Page
-#'
-#' @param text Text of page
-#' @param cols numeric, characters preceding column breaks
-#' @param rows numeric, rows preceding row breaks
-#'
-#' @return a pdf_page object
-#'
-#' str(pdf2tab:::pdf_page(list("HEADER 1         HEADER 2         HEADER 3",
-#'          " adsdsf           asd             asdfad")))
-pdf_page <- function(text, rows = NULL, cols = NULL, split_char = "\n") {
+pdf_page <- function(text,
+                     rows = NULL, cols = NULL, split_char = "\n") {
   structure(list(text = stringr::str_split(text, split_char),
                  cols = cols,
                  rows = rows),
@@ -67,6 +58,9 @@ set_attr.pdf_page <- function(x, which, val) {
 #'
 #' @return x (invisibly)
 #'
+#' @export
+#'
+#' @examples
 #' pdf2tab:::pdf_page(c("a   b   c", "1   2   3"))
 #' pdf2tab:::pdf_page(list("ABCDEF", "123456"), cols = c(2, 4))
 #' pdf2tab:::pdf_page(list("ABCDEF", "ABCDEF", "ABCDEF", "123456"), rows = 3)
@@ -74,28 +68,15 @@ set_attr.pdf_page <- function(x, which, val) {
 #' cols = c(2, 4), rows = c(3, 4))
 print.pdf_page <- function(x, ...) {
 
-  t <- get_page_text(x)
-  t <- add_print_cols(t, get_page_cols(x))
-  t <- add_print_rows(t, get_page_rows(x), get_page_cols(x))
+  t <- get_text(x)
+  t <- add_print_cols(t, get_cols(x))
+  t <- add_print_rows(t, get_rows(x), get_cols(x))
 
-  cat(paste0(t[[1]], collapse = "\n"))
+  cat(paste0(t, collapse = "\n"))
 
   invisible(x)
 }
 
-#' Add column breaks
-#'
-#' @param x list of characters
-#' @param cols column breaks
-#'
-#' @return x with column break characters added
-#'
-#' t <- pdf2tab:::pdf_page(list("123456789123456789",
-#'          "123456789123456789"),
-#'          cols = c(3, 6, 15))
-#' pdf2tab:::add_print_cols(pdf2tab:::get_page_text(t),
-#' pdf2tab:::get_page_cols(t))
-#' pdf2tab:::add_print_cols(pdf2tab:::get_page_text(t), NULL)
 add_print_cols <- function(x, cols) {
   if (is.null(cols)) return(x)
 
@@ -105,16 +86,7 @@ add_print_cols <- function(x, cols) {
                       chars = seq(length(cols)))))
 }
 
-#' Add Rows to Text
-#'
-#' @param x list of characters
-#' @param rows where to add row breaks
-#' @param cols column breaks
-#'
-#' @return x with row break character strings added
-#'
-#' pdf2tab:::add_rows(list("AB|CD|EF", "AB|CD|EF", "AB|CD|EF", "12|34|56"),
-#' rows = c(3, 4))
+
 add_print_rows <- function(x, rows, cols) {
   if (is.null(rows)) return(x)
 
@@ -122,14 +94,6 @@ add_print_rows <- function(x, rows, cols) {
                  make_row(cols, row_len = max(purrr::map_int(x, nchar))))
 }
 
-#' Make filler row
-#'
-#' @param cols location of col breaks
-#' @param row_len total length of row
-#'
-#' @return character of length 1, filler row
-#'
-#' pdf2tab:::make_row(c(2, 4), 6)
 make_row <- function(cols, row_len) {
   r <- rep("-", row_len)
 
@@ -140,32 +104,12 @@ make_row <- function(cols, row_len) {
   paste0(r, collapse = "")
 }
 
-#' Recursively add Rows
-#'
-#' @param x list of characters
-#' @param rows where to add
-#' @param new_row character, value for new row
-#'
-#' @return x with new row added after rows
-#'
-#' pdf2tab:::add_rows_recur(list("ABC", "ABC"), NULL, "")
-#' pdf2tab:::add_rows_recur(list("ABC", "ABC"), 1, "---")
 add_rows_recur <- function(x, rows, new_row) {
   if (length(rows) == 0) return(x)
 
   add_rows_recur(append(x, new_row, rows[1]), rows[-1] + 1, new_row)
 }
 
-#' Add Characters After Specific Number
-#'
-#' @param line character of length 1
-#' @param chars characters to add, length 1 or same as cols
-#' @param cols characters preceding those to add
-#'
-#' @return character, col breaks added after cols
-#'
-#' pdf2tab:::add_chars("ABcdEF", c(2, 4))
-#' pdf2tab:::add_chars("ABcdEF", c(2, 4), 1:2)
 add_chars <- function(line, cols, chars = "|") {
   stopifnot(typeof(line) == "character" & length(line) == 1)
   stopifnot(length(chars) %in% c(1, length(cols)))
